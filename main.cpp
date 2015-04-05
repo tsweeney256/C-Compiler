997830df0eeb06b8d2f97b4e5df3f23182780217
Thomas Sweeney <tsweeney256@gmail.com>
2015-04-05 16:49:43 -0400

    updated the program output for p3

--- main.cpp
+++ main.cpp
@@ -2,14 +2,27 @@
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
+#include <cstring>
 #include <list>
+
 #include "Parser.hpp"
+#include "Tree.hpp"
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2){
-		std::cerr << "Error: Program only accepts a single argument, the location of the input file." << std::endl;
+	bool errorMsgsEnabled = false;
+
+	if(!(argc == 2 || argc == 3)){
+		std::cerr << "Error: Program only accepts one or two arguments: the location of the input file and optionally an error message flag (-e)." << std::endl;
 		exit(1);
+	}
+	if(argc == 3){
+		if(!strcmp("-e", argv[2])){
+			errorMsgsEnabled = true;
+		}
+		else{
+			std::cerr << "unknown argument: " << argv[2] <<std::endl;
+		}
 	}
 	std::fstream inputFile;
 	inputFile.open(argv[1], std::fstream::in);
@@ -17,8 +30,10 @@ int main(int argc, char* argv[])
 		std::cerr << "Error: File does not exist." << std::endl;
 		exit(1);
 	}
-
-	std::cout << Parser::parse(inputFile) << std::endl;
-
-
+	if(Parser::parse(inputFile, errorMsgsEnabled)){
+		std::cout << "ACCEPT" << std::endl;
+	}
+	else{
+		std::cout << "REJECT" << std::endl;
+	}
 }
