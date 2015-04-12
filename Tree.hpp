17dc3ad4b17dfadff16b4e269c02f90926d8a968
Thomas Sweeney <tsweeney256@gmail.com>
2015-04-12 11:41:15 -0400

    Added printing support for Tree and also support for only deleting single nodes without deleting any of its children

--- Tree.hpp
+++ Tree.hpp
@@ -2,7 +2,9 @@
 #define TREE_HPP
 
 #include <cstddef>
+#include <ostream>
 #include <vector>
+#include "SyntaxInfo.hpp"
 
 template <typename T>
 class Tree
@@ -11,13 +13,13 @@ public:
     Tree();
     Tree(const T& val);
     Tree<T>* clone(); //deep copy //not implemented yet
-    static void destroy(Tree<T>* tree);
-
+    static void destroy(Tree<T>* tree); //destroys all children, grandchildren, etc.
+    static void destroyNode(Tree<T>* tree); //doesn't destroy any children
     void connectChild(Tree<T>* child);
-
     int numChildren();
     Tree<T>* getChild(int idx);
     Tree<T>* getParent();
+    void print(std::ostream& stream);
 
     T val;
 
@@ -27,7 +29,7 @@ protected:
 private:
     std::vector<Tree<T>*> m_child;
     Tree<T>* m_parent;
-
+    void print(std::string prefix, bool isTail, std::ostream& os); //recursive function called by printTree();
 };
 
 template <typename T>
@@ -39,15 +41,22 @@ Tree<T>::Tree(const T& val)
         : val(val), m_parent(NULL) {}
 
 template <typename T>
-Tree<T>::~Tree()
-{
-	for(typename std::vector<Tree<T>*>::const_iterator it=m_child.begin(); it != m_child.end(); ++it){
-		delete *it;
-	}
-}
+Tree<T>::~Tree(){}
 
 template <typename T>
 void Tree<T>::destroy(Tree<T>* tree)
+{
+	if(tree){
+		for(typename std::vector<Tree<T>*>::const_iterator it=tree->m_child.begin(); it != tree->m_child.end(); ++it){
+			 destroy(*it);
+		}
+		delete tree;
+	}
+
+}
+
+template <typename T>
+void Tree<T>::destroyNode(Tree<T>* tree)
 {
 	delete tree;
 }
@@ -81,6 +90,27 @@ template <typename T>
 Tree<T>* Tree<T>::getParent()
 {
 	return m_parent;
+}
+
+template <typename T>
+void Tree<T>::print(std::ostream& os)
+{
+	print("", true, os);
+}
+
+//adapted from Vasya Novikov's java implementation
+//http://stackoverflow.com/questions/4965335/how-to-print-binary-tree-diagram
+//I just have this for debugging and it's never actually called in p4, so please don't actually flag it as plagiarism
+template <typename T>
+void Tree<T>::print(std::string prefix, bool isTail, std::ostream& os)
+{
+	os << prefix << (isTail ? "└──" : "├──") << val << std::endl;
+	for(int i=0; i<numChildren()-1; ++i){
+		getChild(i)->print(prefix + (isTail ? "    " : "│   "), false, os);
+	}
+	if(numChildren() > 0){
+		getChild(numChildren()-1)->print(prefix + (isTail ?"    " : "│   "), true, os);
+	}
 }
 
 #endif // TREE_HPP
